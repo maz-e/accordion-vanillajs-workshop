75ac8069524bc03aad767b024a0d8906c0d59422
Miguel Angel Zapatero <m.zapes@gmail.com>
2019-08-24 20:54:14 +0200

    feat: add bonus functionality without Ajax content

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const accordion = document.getElementById('accordion')
+const addBtn = document.getElementById('add-btn')
 let oldTarget
 
 function closeAllAccordionContents() {
     for (let dd of accordion.querySelectorAll('dd')) {
         dd.hidden = true
     }
+}
+
+function addContent() {
+    const total = accordion.children.length / 2
+    let dt = document.createElement('dt')
+    let dd = document.createElement('dd') 
+
+    dt.className = 'Accordion-title'
+    dt.innerHTML = `Section ${total + 1}`
+
+    dd.className = 'Accordion-body'
+    dd.innerHTML = `<p>Section ${total + 1} Content... Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, laboriosam fuga accusamus quis veritatis quo eum consequatur voluptates nemo incidunt sapiente necessitatibus aspernatur vitae minima hic, exercitationem repellat explicabo quaerat?</p>`
+
+    accordion.append(dt)
+    accordion.append(dd)
+    closeAllAccordionContents()
 }
 
 closeAllAccordionContents()
@@ -23,3 +40,4 @@ accordion.onclick = function(event) {
     oldTarget = target
 }
 
+addBtn.addEventListener("click", addContent)
